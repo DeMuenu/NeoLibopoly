8edfc784ca13c15cb8f48f9fc2c8e59d75996c26
DeMuenu <jannisa@gmx.ch>
2023-10-28 01:46:46 +0200

    hmmm

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 
 pygame.init()
 
-ScreenWidth = 1080
-ScreenHeight = 720
+ScreenWidth = 1920
+ScreenHeight = 1080
 
 screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
 
@@ -14,13 +14,19 @@ run = True
 #Textures
 player = pygame.Rect((300, 250, 50, 50))
 
-
+Board = pygame.image.load('assets/Board/doux.png').convert_alpha()
 
 
 
 while run:
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (255, 0, 0), player)
+    screen.blit(Board, (0, 0))
+
+    #walk
+    player.x, player.y = pygame.mouse.get_pos()
+
+
     key = pygame.key.get_pressed()
     if key[pygame.K_w] == True:
         player.move_ip(0, -1)
@@ -31,11 +37,11 @@ while run:
     elif key[pygame.K_d] == True:
         player.move_ip(1, 0)
 
-
     #eventhandler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
 
     pygame.display.update()
 
