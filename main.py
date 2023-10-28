b540faed0d7b39df5655ef6601782ff52fc86a02
DeMuenu <jannisa@gmx.ch>
2023-10-28 12:06:00 +0200

    hjmm

--- main.py
+++ main.py
@@ -9,22 +9,53 @@ ScreenHeight = 1080
 screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
 
 
-run = True
+
+Layer1 = []
+Layer2 = []
+ClickableObjectsList = []
+
+def addRenderObject(object, Layer, StartPosX, StartPosY, Color):
+    if Layer == 1:
+        Layer1.append(emptyDict.copy())
+        x = Layer1[-1]
+    x["object"] = object
+    x["positionX"] = StartPosX
+    x["positionY"] = StartPosY
+    x["Color"] = Color
+
 
 #Textures
+emptyDict = {}
+
 player = pygame.Rect((300, 250, 50, 50))
-
-Board = pygame.image.load('assets/Board/doux.png').convert_alpha()
-
+addRenderObject(player, 1, 900, 800, (0, 0, 255))
 
 
+Board = pygame.image.load('assets/Board/cat.png').convert_alpha()
+addRenderObject(Board, 1, 900, 50, (255, 0, 0))
+ClickableObjectsList.append(Board)
+
+
+
+RenderedObjectsList = [Layer1, Layer2]
+run = True
 while run:
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), player)
-    screen.blit(Board, (0, 0))
+    #pygame.draw.rect(screen, (255, 0, 0), player)
+    #screen.blit(Board, (900, 500))
 
     #walk
-    player.x, player.y = pygame.mouse.get_pos()
+    for z in RenderedObjectsList:
+        for i in z:
+            print(i)
+            if str(i["object"]).startswith("<Surface"):
+                print("Surface")
+                screen.blit(i["object"], (i["positionX"], i["positionY"]))
+
+            if str(i["object"]).startswith("<rect"):
+                print("Rect")
+                pygame.draw.rect(screen, i["Color"], i["object"])
+
 
 
     key = pygame.key.get_pressed()
@@ -41,6 +72,10 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            for i in ClickableObjectsList:
+                if i.get_rect().collidepoint(pygame.mouse.get_pos()):
+                    print("Pressed Clickable Object")
 
 
     pygame.display.update()
