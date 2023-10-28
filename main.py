b4501adffc4a9799c3dfe30028c9ddf9ef214b56
DeMuenu <jannisa@gmx.ch>
2023-10-28 14:16:23 +0200

    movable playing field

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ ScreenHeight = 1080
 screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
 
 
-
+emptyDict = {}
 Layer1 = []
 Layer2 = []
 ClickableObjectsList = []
@@ -18,22 +18,48 @@ def addRenderObject(object, Layer, StartPosX, StartPosY, Color):
     if Layer == 1:
         Layer1.append(emptyDict.copy())
         x = Layer1[-1]
+    if Layer == 2:
+        Layer2.append(emptyDict.copy())
+        x = Layer2[-1]
     x["object"] = object
     x["positionX"] = StartPosX
     x["positionY"] = StartPosY
     x["Color"] = Color
 
-
+def addClickableObject(object, PositionX, PositionY, SizeX, SizeY):
+    ClickableObjectsList.append(emptyDict.copy())
+    x = ClickableObjectsList[-1]
+    x["object"] = object
+    x["positionX"] = PositionX
+    x["positionY"] = PositionY
+    x["SizeX"] = SizeX
+    x["SizeY"] = SizeY
 #Textures
-emptyDict = {}
+
 
 player = pygame.Rect((300, 250, 50, 50))
 addRenderObject(player, 1, 900, 800, (0, 0, 255))
 
 
+PosNextX = 900
+PosNextY = 50
+ScaleNextX = 100
+ScaleNextY = 100
 Board = pygame.image.load('assets/Board/cat.png').convert_alpha()
-addRenderObject(Board, 1, 900, 50, (255, 0, 0))
-ClickableObjectsList.append(Board)
+Board = pygame.transform.scale(Board, (ScaleNextX, ScaleNextY))
+addRenderObject(Board, 1, PosNextX, PosNextY, (255, 0, 0))
+addClickableObject(Board, PosNextX, PosNextY, ScaleNextX, ScaleNextY)
+
+
+
+PosNextX = 900
+PosNextY = 500
+ScaleNextX = 100
+ScaleNextY = 100
+Board = pygame.image.load('assets/Board/cat.png').convert_alpha()
+Board = pygame.transform.scale(Board, (ScaleNextX, ScaleNextY))
+addRenderObject(Board, 1, PosNextX, PosNextY, (255, 0, 0))
+addClickableObject(Board, PosNextX, PosNextY, ScaleNextX, ScaleNextY)
 
 
 
@@ -47,26 +73,49 @@ while run:
     #walk
     for z in RenderedObjectsList:
         for i in z:
-            print(i)
+            #print(i)
             if str(i["object"]).startswith("<Surface"):
-                print("Surface")
+                #print("Surface")
                 screen.blit(i["object"], (i["positionX"], i["positionY"]))
 
             if str(i["object"]).startswith("<rect"):
-                print("Rect")
+                #print("Rect")
                 pygame.draw.rect(screen, i["Color"], i["object"])
 
 
 
     key = pygame.key.get_pressed()
     if key[pygame.K_w] == True:
-        player.move_ip(0, -1)
+        for y in RenderedObjectsList:
+            for i in y:
+                i["positionY"] = i["positionY"] - 1
+
+        for i in ClickableObjectsList:
+            i["positionY"] = i["positionY"] - 1
+
     elif key[pygame.K_s] == True:
-        player.move_ip(0, 1)
+        for y in RenderedObjectsList:
+            for i in y:
+                i["positionY"] = i["positionY"] + 1
+
+        for i in ClickableObjectsList:
+            i["positionY"] = i["positionY"] + 1
+
     elif key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
+        for y in RenderedObjectsList:
+            for i in y:
+                i["positionX"] = i["positionX"] - 1
+
+        for i in ClickableObjectsList:
+            i["positionX"] = i["positionX"] - 1
+
     elif key[pygame.K_d] == True:
-        player.move_ip(1, 0)
+        for y in RenderedObjectsList:
+            for i in y:
+                i["positionX"] = i["positionX"] + 1
+
+        for i in ClickableObjectsList:
+            i["positionX"] = i["positionX"] + 1
 
     #eventhandler
     for event in pygame.event.get():
@@ -74,8 +123,11 @@ while run:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             for i in ClickableObjectsList:
-                if i.get_rect().collidepoint(pygame.mouse.get_pos()):
-                    print("Pressed Clickable Object")
+                print(i)
+                MouseX, MouseY = pygame.mouse.get_pos()
+                if MouseX >= i["positionX"] and MouseX <= i["positionX"] + i["SizeX"] and MouseY >= i["positionY"] and MouseY <= i["positionY"] + i["SizeY"]:
+                    print("Clicked")
+                
 
 
     pygame.display.update()
